c432239b184e1b1340eb61b85cb964aa885206c5
gigashot <147614954+gigashot@users.noreply.github.com>
2024-02-25 13:14:05 +0100

    Update Almanac.py

--- Almanach.py
+++ Almanach.py
@@ -2,7 +2,9 @@
 plant1 = {
 "Name": "Peashooter",
 "Damage": "normal",
-"Description": "yapping",
+"Toughness": "low",
+"Special":"none",
+"Description": "A green Pea-plant that shoots a pea every few seconds.",
 "Cost": 100,
 "Recharge": "fast",
 }
@@ -11,24 +13,29 @@ plant1 = {
 plant2 = {
 "Name": "Squash",
 "Damage": "massive",
-"Description": "AUUHUGA",
+"Toughness": "low",
+"Special":"none",
+"Description": "An angry Squash that crushes anything that gets close.",
 "Cost": 50,
 "Recharge": "slow",
 }
 
 plant3 = {
 "Name": "Sunflower",
-"Sun Production": "normal",
-"Description": "hihi",
+"Damage": "none",
+"Toughness": "low",
+"Special":"sun production",
+"Description": "A happy Sunflower that produces sun over time.",
 "Cost": 50,
 "Recharge": "fast",
 }
 
 plant4 = {
 "Name": "Tall-nut",
+"Damage": "none",
 "Toughness": "very high",
-"Special":"none",
-"Description": "UHUDGIAU",
+"Special":"Can't be vaulted or jumped over",
+"Description": "A tall Wall-nut that protects plants behind him with his own body.",
 "Cost": 125,
 "Recharge": "slow",
 }
@@ -39,9 +46,42 @@ plants = {
 "SUNFLOWER" : plant3,
 "TALL-NUT" : plant4
 }
-for keys in plants:
-    print("--------------------------------------------------")
-    print(keys)
-    for keys2 in plants[keys]:
-        print(keys2, ":", plants[keys][keys2])
-    
+
+def add_New_Plant():
+    new_plant = {}
+
+    new_plant["Name"] = input("Enter the plant name: ")
+    new_plant["Damage"] = input("Enter the damage type: ")
+    new_plant["Toughness"] = input("Enter the toughness level: ")
+    new_plant["Special"] = input("Enter any special ability: ")
+    new_plant["Description"] = input("Enter the plant description: ")
+    new_plant["Cost"] = int(input("Enter the cost: "))
+    new_plant["Recharge"] = input("Enter the recharge time: ")
+
+    plants[new_plant["Name"].upper()] = new_plant
+
+
+def show_All_Plants():
+    for keys in plants:
+        print("--------------------------------------------------")
+        print(keys)
+        for keys2 in plants[keys]:
+            print(keys2, ":", plants[keys][keys2])
+
+def action_Choose():
+    print("Vyberte si funkci co chcete provést: ")
+    print("1) přidat novou rostilu")
+    print("2) ukázat všechny rostliny")
+    print("3) ukončit program")
+    chosen_Option = int(input(""))
+    if chosen_Option == 1:
+        add_New_Plant()
+        action_Choose()
+    elif chosen_Option == 2:
+        show_All_Plants()
+        action_Choose()
+    elif chosen_Option == 3:
+        return print("program ukončen")
+    else:
+        action_Choose()
+action_Choose()
